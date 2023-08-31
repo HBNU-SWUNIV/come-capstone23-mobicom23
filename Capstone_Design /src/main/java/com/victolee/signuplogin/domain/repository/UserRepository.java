ce2ed068e169059791d5ba5a31079bab8195b107
sujin05 <62604226+sujin05@users.noreply.github.com>
2023-08-31 16:37:56 +0900

    Add files via upload

---
+++ Capstone_Design /src/main/java/com/victolee/signuplogin/domain/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.victolee.signuplogin.domain.repository;
+
+
+
+import com.victolee.signuplogin.domain.User;
+import com.victolee.signuplogin.domain.entity.DataEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findAllByEmail(String email);
+}
