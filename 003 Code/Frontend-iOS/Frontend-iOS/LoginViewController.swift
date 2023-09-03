e495a2fad13b22af76c751e50f023bf7d904fd66
DDING <102645399+OhMyungJin@users.noreply.github.com>
2023-09-03 17:58:19 +0900

    코드 복사_1
    기존 로컬에서 구현했던 코드를 복사하는 작업
    git에 올리기 위함

---
+++ 003 Code/Frontend-iOS/Frontend-iOS/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  Frontend-iOS
+//
+//  Created by Mjolnir on 9/3/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var IDTextField: UITextField!
+    @IBOutlet weak var PWTextField: UITextField!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var SignUpButton: UIButton!
+    
+    @IBAction func Login(_ sender: UIButton){
+//        sendGetRequest()
+        
+        let vcSerial = self.storyboard?.instantiateViewController(withIdentifier: "UITabBarController")
+        self.navigationController?.pushViewController(vcSerial!, animated: true)
+    }
+    
+    func sendGetRequest() {
+        guard let url = URL(string: "http://172.17.43.229:8080/login") else {
+            print("URL 생성에 실패했습니다.")
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("요청 실패: \(error.localizedDescription)")
+                return
+            }
+
+            if let data = data, let responseString = String(data: data, encoding: .utf8) {
+                print("응답 받음: \(responseString)")
+            }
+        }
+
+        task.resume()
+    }
+}
+
